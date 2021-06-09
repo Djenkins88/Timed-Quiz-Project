afdb253822a060191952bbf4040dfa5c9fffc802
Dominque Jenkins <dominquejenkins14@gmail.com>
2021-06-09 09:38:51 -0500

    updated js file

---
+++ question.js
@@ -0,0 +1,33 @@
+var quizQuestions = [
+    {
+        quote: "All user-defined objects and built-in objects are descendants of an object called Object?",
+        choices: ["true","false"],
+        answer: "true"
+    },
+    {
+        quote: "Which of the following function of Boolean object returns the primitive value of the Boolean object?",
+        choices: ["toSource()","valueOf()","toString()"," None of the above."],
+        answer: "valueOf()"
+    },  
+    {
+        quote: "Which of the following function of String object extracts a section of a string and returns a new string?",
+        choices: ["slice()","split()","replace()","search()"],
+        answer: "slice()"
+    },
+    {
+        quote: " Which of the following function of String object creates an HTML anchor that is used as a hypertext target?",
+        choices: ["anchor()","link()","blink()","big()"],
+        answer: "anchor()"
+    },
+    {
+        quote: " Which of the following function of String object causes a string to be displayed in the specified color as if it were in a <font color='color'> tag?",
+        choices: ["fixed()","fontcolor()","blink()","bold()"],
+        answer: "bold()"
+    },
+    {
+        quote: "Which of the following function of Array object returns true if every element in this array satisfies the provided testing function?",
+        choices: ["concat()","every()","push()","some()"],
+        answer: "every()"
+    },
+
+];
--- script.js
+++ script.js
@@ -1,21 +1,113 @@
-var btnStart = document.getElementById("btn-start");
-var timerText = document.getElementById("timer-text");
+// variables to manipulate the DOM elements. 
 
-var countdown = 60;
+var btnStartEl = document.getElementById("btn-start");
+var timeLeftEl = document.getElementById("time-left");
+var stContEl = document.getElementById("start-container");
+var scoreEl = document.getElementById("score");
 
-btnStart.addEventListener("click",function() {
-    setInterval(function(){
-      if (countdown <= 0 ) {
-        clearInterval(countdown = 1)
+// variables to control logic state of the quiz
+var quizQuestionIndex = {}
+var answer = true;
+var timer = 75
+var score = 0
+var penalty = 5
+var max_Questions
+
+
+
+//function beginQuiz() {
+// Hide start container
+//stContEl.setAttribute("class", disappear) 
+//}
+
+//btnStartEl.onclick="beginQuiz()";
+
+// This starts the timer 
+btnStartEl.addEventListener('click', function() {
+
+  var startTimer = setInterval(function() {
+      timer--;
+      timeLeftEl.textContent = timer;
+      if(timer === 0) {
+        clearInterval(startTimer);
       }
-      countdown -=1;
-      timerText.textContent = countdown;
-    }, 1000);
+  },1000);
 
-    
+  startQuiz();
 
 });
 
+function startQuiz() {
+  quizQuestionCount = 0
+  availableQuestons = [quizQuestions]
+  getNewQuizQuestion()
+}
+
+function getNewQuizQuestion() {
+  if(availableQuestons.length === 0 || quizQuestioncount > max_Questions) {
+    localStorage.setItem('mostRecentScore', score)
+
+    return window.location.assign('/end.html')
+  }
+
+  var quizQuestionIndex = Math.floor(Math.random() * availableQuestons.length)
+  currentQuestion = availableQuestons[quizQuestionIndex]
+  quizQuestions.innerText = currentQuestion.quizQuestions
 
 
 
+choices.forEach(choice => {
+  choice.addEventListener('click', e => {
+    if(!acceptingAnswers) return
+
+    acceptingAnswers = false
+    var selectedChoice = e.target
+    var selectedAnswer = selectedChoice.dataset['number']
+
+    var classToApply = selectedAnswer == currentQuestion.answer ? 'correct' :
+    'incorrect'
+
+    if (classToApply === 'correct') {
+      incrementScore(score)
+    }
+
+    selectedChoice.parentElement.classList.add(classToApply)
+    
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply)
+      getNewQuizQuestion()
+
+    }, 1000)
+  })
+})
+
+
+
+
+// function nextQuestion() {
+//   questionTitle.textContent = questions[questionIndex].question;
+//   choiceA.textContent = questions[questionIndex].choices[0];
+//   choiceB.textContent = questions[questionIndex].choices[1];
+//   choiceC.textContent = questions[questionIndex].choices[2];
+//   choiceD.textContent = questions[questionIndex].choices[3];
+// }
+
+
+
+
+
+
+
+
+/*function startQuiz() {
+console.log('startingloop');
+  for(var i = 0; i < quizQuestions.length; i++ ){
+    var response = ('quizQestions.answer');
+    if(response == quizQuestions[i].answer){
+      score ++;
+      ('Correct')
+    } else {
+      ('Incorrect')
+    }
+  }
+}*/
